1be5771812b01a5a0d407cfcedcc322016d35a96
ravaattar <rava.naufal2014@gmail.com>
2021-10-30 02:20:38 +0700

    first submission

---
+++ js/main.js
@@ -0,0 +1,126 @@
+// hottest Movie Modal
+const hottest_movie = document.getElementById('hottest_movie');
+const modal_container = document.getElementById('modal_container');
+const close_btn = document.getElementById('close_btn');
+
+
+hottest_movie.addEventListener('click', () => {
+    modal_container.classList.add('show');
+});
+close_btn.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
+// hottest movie modal
+
+// movies modal
+const movie1 = document.getElementById('movie1');
+const movie1_modal = document.getElementById('movie1_modal');
+const close_btn1 = document.getElementById('close_btn1');
+
+movie1.addEventListener('click', () => {
+    movie1_modal.classList.add('show');
+});
+close_btn1.addEventListener('click', () => {
+    movie1_modal.classList.remove('show');
+});
+
+const movie2 = document.getElementById('movie2');
+const movie2_modal = document.getElementById('movie2_modal');
+const close_btn2 = document.getElementById('close_btn2');
+
+movie2.addEventListener('click', () => {
+    movie2_modal.classList.add('show');
+});
+close_btn2.addEventListener('click', () => {
+    movie2_modal.classList.remove('show');
+});
+
+const movie3 = document.getElementById('movie3');
+const movie3_modal = document.getElementById('movie3_modal');
+const close_btn3 = document.getElementById('close_btn3');
+
+movie3.addEventListener('click', () => {
+    movie3_modal.classList.add('show');
+});
+close_btn3.addEventListener('click', () => {
+    movie3_modal.classList.remove('show');
+});
+
+const movie4 = document.getElementById('movie4');
+const movie4_modal = document.getElementById('movie4_modal');
+const close_btn4 = document.getElementById('close_btn4');
+
+movie4.addEventListener('click', () => {
+    movie4_modal.classList.add('show');
+});
+close_btn4.addEventListener('click', () => {
+    movie4_modal.classList.remove('show');
+});
+
+const movie5 = document.getElementById('movie5');
+const movie5_modal = document.getElementById('movie5_modal');
+const close_btn5 = document.getElementById('close_btn5');
+
+movie5.addEventListener('click', () => {
+    movie5_modal.classList.add('show');
+});
+close_btn5.addEventListener('click', () => {
+    movie5_modal.classList.remove('show');
+});
+
+const movie6 = document.getElementById('movie6');
+const movie6_modal = document.getElementById('movie6_modal');
+const close_btn6 = document.getElementById('close_btn6');
+
+movie6.addEventListener('click', () => {
+    movie6_modal.classList.add('show');
+});
+close_btn6.addEventListener('click', () => {
+    movie6_modal.classList.remove('show');
+});
+
+const movie7 = document.getElementById('movie7');
+const movie7_modal = document.getElementById('movie7_modal');
+const close_btn7 = document.getElementById('close_btn7');
+
+movie7.addEventListener('click', () => {
+    movie7_modal.classList.add('show');
+});
+close_btn7.addEventListener('click', () => {
+    movie7_modal.classList.remove('show');
+});
+
+const movie8 = document.getElementById('movie8');
+const movie8_modal = document.getElementById('movie8_modal');
+const close_btn8 = document.getElementById('close_btn8');
+
+movie8.addEventListener('click', () => {
+    movie8_modal.classList.add('show');
+});
+close_btn8.addEventListener('click', () => {
+    movie8_modal.classList.remove('show');
+});
+
+const movie9 = document.getElementById('movie9');
+const movie9_modal = document.getElementById('movie9_modal');
+const close_btn9 = document.getElementById('close_btn9');
+
+movie9.addEventListener('click', () => {
+    movie9_modal.classList.add('show');
+});
+close_btn9.addEventListener('click', () => {
+    movie9_modal.classList.remove('show');
+});
+
+const movie10 = document.getElementById('movie10');
+const movie10_modal = document.getElementById('movie10_modal');
+const close_btn10 = document.getElementById('close_btn10');
+
+movie10.addEventListener('click', () => {
+    movie10_modal.classList.add('show');
+});
+close_btn10.addEventListener('click', () => {
+    movie10_modal.classList.remove('show');
+});
+
+// movies modal
